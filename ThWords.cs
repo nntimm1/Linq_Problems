00e285bdd9b78330a26544dd2d4800f4c2adde37
Nicolas Timm <nntimm1@gmail.com>
2020-02-07 10:51:23 -0600

    LINQ problems created repo with base clases.

---
+++ ThWords.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQProblems
+{
+    class ThWords
+    {
+        // -------------------------------- Member Variable -------------------------------
+
+            List<string> words = new List<string>() { "the", "bike", "this", "it", "tenth", "mathmatics"};
+
+        // -------------------------------- Constructor -----------------------------------
+
+
+
+
+        // -------------------------------- Member Method ---------------------------------
+    }
+}
